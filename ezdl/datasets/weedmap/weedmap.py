836eac8d33bb68502126fa90d8c4ad6f7fff98b1
pasqualedem <pasquale199876@hotmail.it>
2023-06-23 11:20:35 +0200

    weedmap update

--- ezdl/datasets/weedmap/weedmap.py
+++ ezdl/datasets/weedmap/weedmap.py
@@ -14,16 +14,19 @@ from torchvision.transforms import functional as F
 from ezdl.transforms import \
     PairRandomCrop, SegOneHot, ToLong, FixValue, Denormalize, PairRandomFlip, squeeze0, \
     PairFlip, PairFourCrop
-from .sequoia_stats import STATS as SEQUOIA_STATS
-from .rededge_stats import STATS as REDEDGE_STATS
+from wd.data.sequoia_stats import STATS as SEQUOIA_STATS
+from wd.data.rededge_stats import STATS as REDEDGE_STATS
 from sklearn.model_selection import train_test_split
 
 from torch.utils.data.distributed import DistributedSampler
 from super_gradients.training import utils as core_utils
 from super_gradients.training.datasets.mixup import CollateMixup
 from super_gradients.training.exceptions.dataset_exceptions import IllegalDatasetParameterException
+from super_gradients.common.abstractions.abstract_logger import get_logger
 
 from ezdl.data import DatasetInterface
+
+logger = get_logger(__name__)
 
 
 class WeedMapDatasetInterface(DatasetInterface):
@@ -31,6 +34,7 @@ class WeedMapDatasetInterface(DatasetInterface):
         'sequoia': SEQUOIA_STATS,
         'rededge': REDEDGE_STATS
     }
+    size = (5, 256, 256)
 
     def __init__(self, dataset_params, name=None):
         super(WeedMapDatasetInterface, self).__init__(dataset_params)
@@ -41,6 +45,7 @@ class WeedMapDatasetInterface(DatasetInterface):
             if name != os.path.basename(os.path.normpath(dataset_params.root)).lower():
                 raise ValueError(f'Warning! Dataset name should match the dataset, found: {name}, {dataset_params.root}')
         self.dataset_name = name
+        self.size = (len(channels), ) + self.size[1:]
 
         mean, std = self.get_mean_std(dataset_params['train_folders'], channels, name)
 
@@ -62,16 +67,14 @@ class WeedMapDatasetInterface(DatasetInterface):
             transforms.PILToTensor(),
             squeeze0,
             ToLong(),
-            FixValue(source=10000, target=1),
-            SegOneHot(num_classes=len(WeedMapDataset.CLASS_LABELS.keys()))
+            FixValue(source=10000, target=1)
         ]
 
         target_transform = [
             transforms.PILToTensor(),
             squeeze0,
             ToLong(),
-            FixValue(source=10000, target=1),
-            SegOneHot(num_classes=len(WeedMapDataset.CLASS_LABELS.keys()))
+            FixValue(source=10000, target=1)
         ]
         period = 1
 
@@ -155,6 +158,100 @@ class WeedMapDatasetInterface(DatasetInterface):
             stds = [np.sqrt((sums[c + '_sum_sq'] / count) - (means[i] ** 2))
                     for i, c in enumerate(channels)]
             return means, stds
+
+    def build_data_loaders(self, batch_size_factor=1, num_workers=8, train_batch_size=None, val_batch_size=None,
+                           test_batch_size=None, distributed_sampler: bool = False):
+        """
+
+        define train, val (and optionally test) loaders. The method deals separately with distributed training and standard
+        (non distributed, or parallel training). In the case of distributed training we need to rely on distributed
+        samplers.
+        :param batch_size_factor: int - factor to multiply the batch size (usually for multi gpu)
+        :param num_workers: int - number of workers (parallel processes) for dataloaders
+        :param train_batch_size: int - batch size for train loader, if None will be taken from dataset_params
+        :param val_batch_size: int - batch size for val loader, if None will be taken from dataset_params
+        :param distributed_sampler: boolean flag for distributed training mode
+        :return: train_loader, val_loader, classes: list of classes
+        """
+        # CHANGE THE BATCH SIZE ACCORDING TO THE NUMBER OF DEVICES - ONLY IN NON-DISTRIBUED TRAINING MODE
+        # IN DISTRIBUTED MODE WE NEED DISTRIBUTED SAMPLERS
+        # NO SHUFFLE IN DISTRIBUTED TRAINING
+        if distributed_sampler:
+            self.batch_size_factor = 1
+            train_sampler = DistributedSampler(self.trainset)
+            val_sampler = DistributedSampler(self.valset)
+            test_sampler = DistributedSampler(self.testset) if self.testset is not None else None
+            train_shuffle = False
+        else:
+            self.batch_size_factor = batch_size_factor
+            train_sampler = None
+            val_sampler = None
+            test_sampler = None
+            train_shuffle = True
+
+        if train_batch_size is None:
+            train_batch_size = self.dataset_params.batch_size * self.batch_size_factor
+        if val_batch_size is None:
+            val_batch_size = self.dataset_params.val_batch_size * self.batch_size_factor
+        if test_batch_size is None:
+            test_batch_size = self.dataset_params.test_batch_size * self.batch_size_factor
+
+        train_loader_drop_last = core_utils.get_param(self.dataset_params, 'train_loader_drop_last', default_val=False)
+
+        cutmix = core_utils.get_param(self.dataset_params, 'cutmix', False)
+        cutmix_params = core_utils.get_param(self.dataset_params, 'cutmix_params')
+
+        # WRAPPING collate_fn
+        train_collate_fn = core_utils.get_param(self.trainset, 'collate_fn')
+        val_collate_fn = core_utils.get_param(self.valset, 'collate_fn')
+        test_collate_fn = core_utils.get_param(self.testset, 'collate_fn')
+
+        if cutmix and train_collate_fn is not None:
+            raise IllegalDatasetParameterException("cutmix and collate function cannot be used together")
+
+        if cutmix:
+            # FIXME - cutmix should be available only in classification dataset. once we make sure all classification
+            # datasets inherit from the same super class, we should move cutmix code to that class
+            logger.warning("Cutmix/mixup was enabled. This feature is currently supported only "
+                           "for classification datasets.")
+            train_collate_fn = CollateMixup(**cutmix_params)
+
+        # FIXME - UNDERSTAND IF THE num_replicas VARIBALE IS NEEDED
+        # train_sampler = DistributedSampler(self.trainset,
+        #                                    num_replicas=distributed_gpus_num) if distributed_sampler else None
+        # val_sampler = DistributedSampler(self.valset,
+        #                                   num_replicas=distributed_gpus_num) if distributed_sampler else None
+        pw = num_workers > 0
+        self.train_loader = torch.utils.data.DataLoader(self.trainset,
+                                                        batch_size=train_batch_size,
+                                                        shuffle=train_shuffle,
+                                                        num_workers=num_workers,
+                                                        pin_memory=True,
+                                                        sampler=train_sampler,
+                                                        collate_fn=train_collate_fn,
+                                                        drop_last=train_loader_drop_last,
+                                                        persistent_workers=pw)
+
+        self.val_loader = torch.utils.data.DataLoader(self.valset,
+                                                      batch_size=val_batch_size,
+                                                      shuffle=False,
+                                                      num_workers=num_workers,
+                                                      pin_memory=True,
+                                                      sampler=val_sampler,
+                                                      collate_fn=val_collate_fn,
+                                                      persistent_workers=pw)
+
+        if self.testset is not None:
+            self.test_loader = torch.utils.data.DataLoader(self.testset,
+                                                           batch_size=test_batch_size,
+                                                           shuffle=False,
+                                                           num_workers=num_workers,
+                                                           pin_memory=True,
+                                                           sampler=test_sampler,
+                                                           collate_fn=test_collate_fn,
+                                                           persistent_workers=pw)
+
+        self.classes = self.trainset.classes
 
     def get_run_loader(self, root=None, folders=None, batch_size=None):
         batch_size = batch_size if batch_size is not None else self.dataset_params.test_batch_size
